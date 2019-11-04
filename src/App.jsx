993a75c5c43bb118da87c885932c0e860a453e3c
Dimitar Delov <dimitar.delov24@gmail.com>
2019-11-04 20:51:44 +0200

    Added Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-      <h2>Two</h2>
-      <h3>Three 223</h3>
-    </div>
+import ThemeContext, { LIGHT_THEME, DARK_THEME } from './context/ThemeContext';
+import { themify } from './utils/themify';
+import Navigation from './components/Navigation/Navigation';
+import styles from './App.module.css';
+
+const App = () => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') || LIGHT_THEME,
   );
-}
+
+  const toggleTheme = () => {
+    const newTheme = theme === LIGHT_THEME ? DARK_THEME : LIGHT_THEME;
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
+
+  return (
+    <ThemeContext.Provider value={theme}>
+      <div className={styles[themify('appWrapper', theme)]}>
+        <BrowserRouter>
+          <Navigation toggleTheme={toggleTheme} />
+          <Switch></Switch>
+        </BrowserRouter>
+      </div>
+    </ThemeContext.Provider>
+  );
+};
 
 export default App;
