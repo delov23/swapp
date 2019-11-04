ba8e6370c2da797ff01210cedaee713a31c39546
Dimitar Delov <dimitar.delov24@gmail.com>
2019-11-04 21:11:19 +0200

    CSS working.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import './css/index.css';
+import './fonts/SfDistantGalaxyAlternate-OE4d.ttf';
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
