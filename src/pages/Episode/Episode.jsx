a6805b53a9d87f46f77e26872975227b10cacfd7
Dimitar Delov <dimitar.delov24@gmail.com>
2019-11-09 10:53:50 +0200

    Data fetching added.

--- src/pages/Episode/Episode.jsx
+++ src/pages/Episode/Episode.jsx
@@ -1,9 +1,79 @@
 import React from 'react';
+import gql from 'graphql-tag.macro';
+import { useQuery } from '@apollo/react-hooks';
+import { Loading } from '../../components/Loading';
+
+export const EPISODE_QUERY = gql`
+  query episode($id: ID!, $first: Int!, $after: String) {
+    episode(id: $id) {
+      id
+      title
+      openingCrawl
+      director
+      releaseDate
+      episodeId
+      image
+      people(first: $first, after: $after) {
+        edges {
+          node {
+            id
+            name
+            image
+          }
+          cursor
+        }
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+      }
+    }
+  }
+`;
 
 const Episode = ({ match }) => {
-  console.log(match.params);
+  const { loading, error, data, fetchMore } = useQuery(EPISODE_QUERY, {
+    variables: {
+      first: 5,
+      id: match.params.episodeId,
+    },
+  });
 
-  return <div>{JSON.stringify(match.params)}</div>;
+  const handleMore = () => {
+    fetchMore({
+      variables: { after: data.episode.people.pageInfo.endCursor },
+      updateQuery: (prev, { fetchMoreResult: { episode } }) => {
+        return {
+          episode: {
+            ...episode,
+            people: {
+              ...episode.people,
+              edges: [...prev.episode.people.edges, ...episode.people.edges],
+            },
+          },
+        };
+      },
+    });
+  };
+
+  if (loading) return <Loading />;
+  if (error) return <div>err</div>;
+
+  const {
+    episode: { people, title },
+  } = data;
+
+  return (
+    <>
+      <div>
+        <h1>{title}</h1>
+        {people.edges.map(({ node }) => (
+          <h3 key={node.id}>{node.name}</h3>
+        ))}
+      </div>
+      <button onClick={handleMore}>Load more</button>
+    </>
+  );
 };
 
 export default Episode;
